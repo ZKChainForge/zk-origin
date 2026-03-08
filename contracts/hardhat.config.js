d0b6eb8b0db433ad24a69ee42b730ebf7f2a126a
ZKChainForge <zkchainforge@gmail.com>
2026-03-08 08:11:09 +0530

    hardhat configuration

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28",
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",   // points to contracts/contracts
+  },
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.SEPOLIA_PRIVATE_KEY
+        ? [process.env.SEPOLIA_PRIVATE_KEY]
+        : [],
+    },
+  },
+};
